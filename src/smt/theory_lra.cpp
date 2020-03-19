9b58b772746663ec7b94436190b8d7c6e4d3412c
Nikolaj Bjorner <nbjorner@microsoft.com>
2020-03-18 18:33:02 -0700

    remove debug out
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- src/smt/theory_lra.cpp
+++ src/smt/theory_lra.cpp
@@ -3206,7 +3206,6 @@ public:
     // lp::constraint_index const null_constraint_index = UINT_MAX; // not sure what a correct fix is
 
     void set_evidence(lp::constraint_index idx) {
-        std::cout << idx << "\n";
         if (idx == UINT_MAX) {
             return;
         }
@@ -3224,7 +3223,6 @@ public:
             break;
         }
         case definition_source: {
-            std::cout << "def\n";
             // skip definitions (these are treated as hard constraints)
             break;
         }
@@ -3262,7 +3260,6 @@ public:
                 set_evidence(ev.second);
             }
         }
-        std::cout << m_core << "\n";
         // SASSERT(validate_conflict());
         dump_conflict();
         if (is_conflict) {
